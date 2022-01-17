8a2ebae189514591a6c2485d7928a4ad96125457
gaev <gaev@cenne.xyz>
2022-01-17 03:37:43 +0100

    update: routes list

--- src/v1/index.js
+++ src/v1/index.js
@@ -6,24 +6,31 @@ const catchAsyncErrors = require("./middlewares/catchAsyncErrors");
 
 const components = require("./components");
 
+
 router.get('/api/v1', (_, response) => {
   response.status(200).json({
     message: 'Bienvenue sur Refemi API (v1)',
     endpoints: [
-      '/api/v1/sections',
-      '/api/v1/categories',
-      '/api/v1/theme',
-      '/api/v1/references',
-      '/api/v1/auth'
+      { ['GET']: '/api/v1/sections' },
+      { ['GET, POST, PUT, DELETE']: '/api/v1/sections/:id' },
+      { ['GET']: '/api/v1/categories' },
+      { ['GET, POST, PUT, DELETE']: '/api/v1/categories/:id' },
+      { ['GET']: '/api/v1/categories/sections/:section_id' },
+      { ['GET']: '/api/v1/themes' },
+      { ['GET, POST, PUT, DELETE']: '/api/v1/themes/:id' },
+      { ['GET']: '/api/v1/references' },
+      { ['GET, POST, PUT, DELETE']: '/api/v1/references/:id' },
+      { ['GET']: '/api/v1/references/section/:id' },
+      { ['GET']: '/api/v1/references/theme/:id' },
+      { GET: '/api/v1/auth' }
     ]
   });
 });
 
 router.use('/api/v1',
+  catchAsyncErrors(async (request, response, next) => components(request, response, next)),
   errorMiddleware,
-  catchAsyncErrors(async (request, response, next) =>
-      components(request, response, next)
-  )
 );
+
 
 module.exports = router;
