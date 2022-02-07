72357787023ab26069e2d35f518d13998ba765b0
gaev <gaev@cenne.xyz>
2022-02-07 12:05:09 +0100

    update: message and error forwarding

--- src/v1/components/auth/authErrors.js
+++ src/v1/components/auth/authErrors.js
@@ -1,33 +1,34 @@
 const ErrorHandler = require("../../classes/ErrorHandler");
 
-class ErrorUserNotFound extends ErrorHandler {
-  constructor() {
-    super("User not found", 404);
-  }
-}
-
+// If the login information, username, or password,
+// does not match what is registered in the database
 class ErrorUserCredential extends ErrorHandler {
   constructor() {
-    super("User credentials is incorrect", 401);
+    super("Le nom d'utilisateur, ou le mot de passe est inccorect", 401);
   }
 }
-
+// If the sent password does not respect the rules of the regex
 class ErrorUserPassword extends ErrorHandler {
   constructor() {
-    super("User credentials is incorrect", 401);
+    super(`Le mot de passe doit avoir au moins 6 caractères, dont au moins une majuscule et une minuscule, et un chiffre, et un caractère spécial`, 401);
   }
 }
-
-class ErrorUserExist extends ErrorHandler {
+// If the user or the email address already exists
+class ErrorNewUser extends ErrorHandler {
   constructor() {
-    super("User already exists", 409);
+    super("Impossible de créer un compte avec ces informations", 409);
+  }
+}
+class ErrorNewUserMissingCredential extends ErrorHandler {
+  constructor() {
+    super("Le nom d'utilisateur, le mot de passe, et le couriel sont requis", 400);
   }
 }
 
 module.exports = {
   ErrorHandler,
-  ErrorUserNotFound,
   ErrorUserCredential,
   ErrorUserPassword,
-  ErrorUserExist,
+  ErrorNewUser,
+  ErrorNewUserMissingCredential
 };
