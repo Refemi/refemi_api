d7adafee978c8dc5539d32422aaac172813e3efa
Laura Illouz <lauraillouz.contact@gmail.com>
2023-02-27 20:00:16 +0100

    test 4

--- src/v1/components/themes/themesRouters.js
+++ src/v1/components/themes/themesRouters.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const ThemesController = require("./themesController");
 
-router.get("/", ThemesController.getAllThemes);
+router.get(
+  "/",
+  (_, response) =>
+    response
+      .status(200)
+      .json({ coucou: "coucou" }) /*ThemesController.getAllThemes*/
+);
 
 module.exports = router;
