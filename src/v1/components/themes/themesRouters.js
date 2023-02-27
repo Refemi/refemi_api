1ed41f9733c72b607ef76d2466a265d8a10f6e55
Laura Illouz <lauraillouz.contact@gmail.com>
2023-02-27 21:49:06 +0100

    remove router test

--- src/v1/components/themes/themesRouters.js
+++ src/v1/components/themes/themesRouters.js
@@ -3,12 +3,6 @@ const router = express.Router();
 
 const ThemesController = require("./themesController");
 
-router.get(
-  "/",
-  (_, response) =>
-    response
-      .status(200)
-      .json({ coucou: "coucou" }) /*ThemesController.getAllThemes*/
-);
+router.get("/", ThemesController.getAllThemes);
 
 module.exports = router;
